fb0042d71988a4dcdfb73ddf56b8d8f15ae9b02c
Xutrurn <kanjoskillz@gmail.com>
2021-01-08 17:58:04 +0100

    js pulsante indietro funzionante

--- js/script.js
+++ js/script.js
@@ -13,13 +13,22 @@ $(document).ready(function() {
 function pulsanteNext() {
 
   var imgAttiva = $(".images img.active");
+  var pallinoAttivo = $(".nav i.active");
 
   imgAttiva.removeClass("active");
   imgAttiva.next().addClass("active");
 
+  pallinoAttivo.removeClass("active");
+  pallinoAttivo.next().addClass("active");
+
   if (imgAttiva.hasClass("last")) {
     $(".images img.first").addClass("active");
+    $(".nav i.first").addClass("active");
   }
+};
+
+function pulsantePrev() {
+
 };
 
 
