75f7846c696611fd509487aeaed2d5c2320881dc
Xutrurn <kanjoskillz@gmail.com>
2021-01-08 17:03:05 +0100

    js pulsante avanti funzionante

--- js/script.js
+++ js/script.js
@@ -4,12 +4,23 @@
 // Soluzione 1
 $(document).ready(function() {
 
+  $(".next").click(pulsanteNext);
 
 
 });
 
 
+function pulsanteNext() {
 
+  var imgAttiva = $(".images img.active");
+
+  imgAttiva.removeClass("active");
+  imgAttiva.next().addClass("active");
+
+  if (imgAttiva.hasClass("last")) {
+    $(".images img.first").addClass("active");
+  }
+};
 
 
 // console.log();
