10046aa85cfa8941221c5db50e8b80dd99f7601a
Xutrurn <kanjoskillz@gmail.com>
2021-01-10 10:22:17 +0100

    aggiunto scorrimento con tastiera

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 // alert("Hello world");
 
 
-// Soluzione 1
 $(document).ready(function() {
 
   $(".next").click(pulsanteNext);
-
+  $(".prev").click(pulsantePrev);
+  $(".nav i").click(pallino);
+  $("body").keydown(tastiera);
 
 });
-
 
 function pulsanteNext() {
 
@@ -25,11 +25,49 @@ function pulsanteNext() {
     $(".images img.first").addClass("active");
     $(".nav i.first").addClass("active");
   }
+
 };
 
 function pulsantePrev() {
 
+  var imgAttiva = $(".images img.active");
+  var pallinoAttivo = $(".nav i.active");
+
+  imgAttiva.removeClass("active");
+  imgAttiva.prev().addClass("active");
+
+  pallinoAttivo.removeClass("active");
+  pallinoAttivo.prev().addClass("active");
+
+
+  if (imgAttiva.hasClass("first")) {
+    $(".images img.last").addClass("active");
+    $(".nav i.last").addClass("active");
+  }
+
 };
+
+function pallino() {
+
+  $('.nav i.active').removeClass('active');
+  $(this).addClass('active');
+
+  var indicePallino = $(this).index();
+  $('.images img').removeClass('active');
+  $('.images img').eq(indicePallino).addClass('active');
+};
+
+function tastiera(valore) {
+  var key = valore.keyCode;
+
+  if (key === 39) {
+    pulsanteNext();
+  } else if (key === 37) {
+    pulsantePrev();
+  }
+};
+
+
 
 
 // console.log();
